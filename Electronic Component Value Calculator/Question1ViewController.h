368dd62dbaeeb0f1aeebfb02fad0f85da92283de
cashie <cashie@pauls-MacBook-Air.local>
2017-11-30 22:48:46 +0000

    I have added quiz view controllers and buttons and labels. and their connection in the storyboard

---
+++ Electronic Component Value Calculator/Question1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Electronic Component Value Calculator
+//
+//  Created by cashie on 28/11/2017.
+//  Copyright © 2017 leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Question1ViewController : UIViewController
+
+@end
