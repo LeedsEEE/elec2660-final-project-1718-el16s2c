62f41d6b6479f675999e3dae1b181ad9b2231f84
cashie <cashie@campus-10-41-65-80.wireless.leeds.ac.uk>
2017-12-07 23:04:48 +0000

    segue has been updated

--- Electronic Component Value Calculator/SecondViewController.m
+++ Electronic Component Value Calculator/SecondViewController.m
@@ -188,6 +188,25 @@
     display.text = [NSString stringWithFormat:@"%@±10%%",display.text];
 }
 
+#pragma mark - Warning for empty text field
+
+-(void)showMessage{
+    
+        UIAlertController * warning =   [UIAlertController
+                                         alertControllerWithTitle:@"Warning!"
+                                         
+                                         message:@"Please Select 4 Colours From the Screen"
+                                         preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){// This code is adapted from https://stackoverflow.com/questions/32690086/uialertview-first-deprecated-ios-9
+            
+            //do something when click button
+            return ;}];
+        [warning addAction:okAction];
+        UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        
+        [vc presentViewController:warning animated:YES completion:nil];
+    }
 
 
 - (IBAction)Clear:(id)sender {
